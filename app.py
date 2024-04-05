2a1a77ba8b05ea180ac0533b0bd3b4332f61a2df
nugosvianadze <nugzar.svianadze.1@btu.edu.ge>
2024-04-05 15:13:40 +0400

    added about page route

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def hello_world():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return 'About page'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
